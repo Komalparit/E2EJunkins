6c3216ae583b41e4acf3184bf2841b3e4a80a872
komal <komal@LAPTOP-U8RE5VSE>
2021-08-05 21:18:18 +0530

    Created Automation Project

---
+++ src/TestCases/Tc_03.java
@@ -0,0 +1,20 @@
+package TestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Tc_03 {
+	@Test
+	public void testcase1() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\komal\\OneDrive\\Desktop\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.quit();
+	}
+}
